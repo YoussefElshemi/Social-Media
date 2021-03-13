060e4388c2860cbd98b4f6e3e19f132e3949dcae
Youssef Elshemi <ye216@exeter.ac.uk>
2021-03-13 13:27:05 +0000

    Adding Account class

---
+++ src/socialmedia/SocialMediaAccount.java
@@ -0,0 +1,33 @@
+package socialmedia;
+
+public class SocialMediaAccount {
+  
+  private static int count = 0;
+  private int id;
+  private String handle;
+
+  public SocialMediaAccount(String handle) throws IllegalHandleException {
+    if (handle.length() > 30 || handle.isBlank() || handle.contains(" ")) {
+      throw new IllegalHandleException();
+    }
+    
+    this.handle = handle;
+    this.id = ++count;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getHandle() {
+    return handle;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setHandle(String handle) {
+    this.handle = handle;
+  }
+}
